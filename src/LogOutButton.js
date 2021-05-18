50d31d3e7dd7acc5e933d0368917fbd0e7abf153
Pranand <pranandv@gmail.com>
2021-05-18 19:36:13 +0530

    Final commit

---
+++ src/LogOutButton.js
@@ -0,0 +1,16 @@
+import react from 'react';
+import { useAuth0} from '@auth0/auth0-react'
+
+const LogOutButton=()=>{
+    
+    const { logout, isAuthenticated } = useAuth0();
+    
+    return(
+        isAuthenticated && (
+        <button onClick={()=>logout({returnTo:window.location.origin
+         } )}>Log Out</button>
+        )
+    )
+}
+
+export default LogOutButton;
